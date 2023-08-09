02dcbd324719d9f45873e70d0b97ab08ddb0bb70
Yasin <yasincetin2014@gmail.com>
2023-08-09 13:22:58 +0300

    initial commit

---
+++ FlashFy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashFy
+//
+//  Created by Yasin Cetin on 9.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
