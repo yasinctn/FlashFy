2b213ec2baf4ca9188164fb3b8a77efacfd6d50c
Yasin <yasincetin2014@gmail.com>
2023-08-14 20:32:10 +0300

    User interface created

---
+++ FlashFy/Views/CategoryCell/CategoryCell.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCell.swift
+//  FlashFy
+//
+//  Created by Yasin Cetin on 14.08.2023.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+}
---
+++ FlashFy/Views/NewsCell/NewsCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewsCell.swift
+//  FlashFy
+//
+//  Created by Yasin Cetin on 14.08.2023.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
